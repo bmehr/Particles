f19f7025b3baed67fee8ccc95bccd1ddb4945eac
Bryan Mehr <bryanmehr@gmail.com>
2025-06-04 16:16:29 -0700

    Update main.js-adjust attractor strength down

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ gui.add(settings, 'particleCount', 100, 20000).step(100).name('Count').onChange(
   rebuildParticles();
 });
 gui.add(settings, 'attractorEnabled').name('Enable Attractor');
-gui.add(settings, 'attractorStrength', 0.0, 0.01).step(0.0001).name('Attractor Strength');
+gui.add(settings, 'attractorStrength', 0.00001, 0.01).step(0.00001).name('Attractor Strength');
 gui.add(settings, 'showUI').name('Show/Hide').onChange(v => v ? gui.show() : gui.hide());
 
 function hexToRGB(hex) {
